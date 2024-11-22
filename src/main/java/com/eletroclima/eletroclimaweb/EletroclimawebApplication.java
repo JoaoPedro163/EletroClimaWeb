12dc122b9a144fb697628685eaf44ffc38f303b2
JoaoPedro163 <joaopedrosilvamendes1@gmail.com>
2024-11-22 06:18:13 -0300

    Criação de todos html e começo de estilização do site

---
+++ src/main/java/com/eletroclima/eletroclimaweb/EletroclimawebApplication.java
@@ -0,0 +1,13 @@
+package com.eletroclima.eletroclimaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EletroclimawebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EletroclimawebApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/eletroclima/eletroclimaweb/controller/EletroClimaController.java
@@ -0,0 +1,29 @@
+package com.eletroclima.eletroclimaweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EletroClimaController {
+    
+    
+    @GetMapping("/eletroclima")
+    public String abrirIndex(){
+        return "index";
+    }
+    
+    @GetMapping("/calculo-btus")
+    public String abrirCalculo(){
+        return "calculoBtus";
+    }
+    
+    @GetMapping("/modelo")
+    public String abrirModelo(){
+        return "modelo";
+    }
+
+    @GetMapping("/listagem-modelos")
+    public String abrirListagem(){
+        return "listagemModelos";
+    }
+}
